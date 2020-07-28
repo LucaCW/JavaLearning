e729f4b00ceba3ffc34777735e1778257efc483e
LucaCW <email.office45@yahoo.com>
2020-07-28 20:26:09 +0200

    Update

--- src/de/luca/javalearning/Calculator.java
+++ src/de/luca/javalearning/Calculator.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * @author Luca R.
  */
 
-public class calculator {
+public class Calculator {
 
     public static void main(String[] args) {
         //Deklarieren der Variablen
---
+++ src/de/luca/javalearning/Strings.java
@@ -0,0 +1,25 @@
+package de.luca.javalearning;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        char [] a = {'c', 'o', 'o', 'l'};
+        String b = new String(a);
+
+        String besser = "cool";
+
+        System.out.println(b);
+        System.out.println(besser);
+
+        String d = "Das ";
+        String e = "ist ";
+        String f = "cool.";
+        String def = d+e+f;
+
+        int laenge = def.length();
+
+        System.out.println(def);
+        System.out.println("Laenge: " + laenge);
+    }
+
+}
